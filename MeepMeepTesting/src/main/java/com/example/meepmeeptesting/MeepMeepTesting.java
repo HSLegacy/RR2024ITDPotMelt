d476421455de750d38a03f413f6265212e1d51d6
Henry <henry.seefeldt@ums.legacypca.org>
2024-10-22 14:53:50 -0500

    Added Pathways *fix starting position *fix turning in the initial phases

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -10,16 +10,23 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        Pose2d startPos = new Pose2d(37, 58, 0);
+        Pose2d startPose = new Pose2d(37, 58, 0);
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(startPos)
-                .lineToX(56)
-                .lineToX(startPos.position.x)
-                .strafeTo(new Vector2d(startPos.position.x, 25))
+        Pose2d bucketPose = new Pose2d(56, startPose.position.y, 180);
+        myBot.runAction(myBot.getDrive().actionBuilder(startPose)
+                .strafeToLinearHeading(bucketPose.position, bucketPose.heading)
+                .strafeToLinearHeading(startPose.position, startPose.heading)
+                .strafeTo(new Vector2d(startPose.position.x, 25))
+                .strafeTo(new Vector2d(56, 58))
+                //second sample
+                .strafeTo(new Vector2d(50, 25))
+                .strafeTo(bucketPose.position)
+                .strafeTo(new Vector2d(60, 25))
+                .strafeTo(bucketPose.position)
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)

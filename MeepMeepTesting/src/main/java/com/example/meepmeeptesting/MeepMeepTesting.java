9f4a88a07b40c5fd5a787c9086d89b6a10e5d193
Henry <henry.seefeldt@ums.legacypca.org>
2024-10-24 14:22:36 -0500

    General Pathways completed, needed tuning for new robot

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -5,28 +5,32 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
-
+import java.lang.Math;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
-
-        Pose2d startPose = new Pose2d(37, 58, 0);
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        Pose2d bucketPose = new Pose2d(56, startPose.position.y, 180);
+
+        Pose2d startPose = new Pose2d(37, 63, Math.toRadians(180));
+        Pose2d bucketPose = new Pose2d(56, 58, 180);
+
         myBot.runAction(myBot.getDrive().actionBuilder(startPose)
+                .strafeTo(new Vector2d(startPose.position.x, startPose.position.y - 10))
+                //pre-loaded sample
                 .strafeToLinearHeading(bucketPose.position, bucketPose.heading)
-                .strafeToLinearHeading(startPose.position, startPose.heading)
-                .strafeTo(new Vector2d(startPose.position.x, 25))
-                .strafeTo(new Vector2d(56, 58))
+                //first sample
+                .strafeToLinearHeading(new Vector2d(37, 25), 0)
+                .strafeToLinearHeading(bucketPose.position, bucketPose.heading)
                 //second sample
-                .strafeTo(new Vector2d(50, 25))
-                .strafeTo(bucketPose.position)
-                .strafeTo(new Vector2d(60, 25))
-                .strafeTo(bucketPose.position)
+                .strafeToLinearHeading(new Vector2d(50, 25), 0)
+                .strafeToLinearHeading(bucketPose.position, bucketPose.heading)
+                // third sample
+                .strafeToLinearHeading(new Vector2d(60, 25), 0)
+                .strafeToLinearHeading(bucketPose.position, bucketPose.heading)
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Net_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Net_Auto.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.auto;
-import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
@@ -10,15 +8,12 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
 @Config
 @Autonomous(name = "BLUE_TEST_AUTO_PIXEL", group = "Autonomous")
-public class testAuto extends LinearOpMode {
+public class Net_Auto extends LinearOpMode {
 /*
     public class ArmActions {
         CRServo sub_extender;
@@ -66,20 +61,24 @@ public class testAuto extends LinearOpMode {
 
         //ArmActions  Arm = new ArmActions(hardwareMap);
 
-        Pose2d startPose = new Pose2d(37, 58, 0);
+        Pose2d startPose = new Pose2d(37, 63, Math.toRadians(180));
+        Pose2d bucketPose = new Pose2d(56, 58, 180);
+
         MecanumDrive drive = new MecanumDrive(hardwareMap, startPose);
 
-        Pose2d bucketPose = new Pose2d(56, startPose.position.y, 180);
         TrajectoryActionBuilder tab1 = drive.actionBuilder(startPose)
+                .strafeTo(new Vector2d(startPose.position.x, startPose.position.y - 10))
+                //pre-loaded sample
                 .strafeToLinearHeading(bucketPose.position, bucketPose.heading)
-                .strafeToLinearHeading(startPose.position, startPose.heading)
-                .strafeTo(new Vector2d(startPose.position.x, 25))
-                .strafeTo(new Vector2d(56, 58))
+                //first sample
+                .strafeToLinearHeading(new Vector2d(37, 25), 0)
+                .strafeToLinearHeading(bucketPose.position, bucketPose.heading)
                 //second sample
-                .strafeTo(new Vector2d(50, 25))
-                .strafeTo(bucketPose.position)
-                .strafeTo(new Vector2d(60, 25))
-                .strafeTo(bucketPose.position);
+                .strafeToLinearHeading(new Vector2d(50, 25), 0)
+                .strafeToLinearHeading(bucketPose.position, bucketPose.heading)
+                // third sample
+                .strafeToLinearHeading(new Vector2d(60, 25), 0)
+                .strafeToLinearHeading(bucketPose.position, bucketPose.heading);
 
 
         while (!isStopRequested() && !opModeIsActive()) {
